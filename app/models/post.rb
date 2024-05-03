72f85265a5dbbf1807267796f1dadbbffae8dba6
Anjasfedo <fedoaf98@gmail.com>
2024-05-03 15:09:36 +0700

    Attach Qrcode Image when Create & Update

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,42 @@
 class Post < ApplicationRecord
-    # include Rails.application.routes.url_helpers
+  has_one_attached :qrcode, dependent: :destroy
 
-    has_one_attached :qrcode, dependent: :destroy
+  before_create :generate_qrcode
+  before_update :regenerate_qrcode, if: :title_changed?
 
-    private
+  private
 
-    def generate_qrcode
-        
-    end
+  def generate_qrcode
+    generate_and_attach_qrcode if title.present?
+  end
+
+  def regenerate_qrcode
+    qrcode.purge if qrcode.attached?
+    generate_and_attach_qrcode
+  end
+
+  def generate_and_attach_qrcode
+    require 'rqrcode'
+
+    qrcode_data = RQRCode::QRCode.new(title)
+
+    png = qrcode_data.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: 'black',
+      file: nil,
+      fill: 'white',
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_gte_to: false,
+      size: 120
+    )
+
+    qrcode.attach(
+      io: StringIO.new(png.to_s),
+      filename: 'qrcode.png',
+      content_type: 'image/png'
+    )
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  # config.action_controller.default_url_options = { host: "localhost:3000", port: 3000 }
+  # config.action_controller.default_url_options = { host: "localhost", port: 3000 }
 end
